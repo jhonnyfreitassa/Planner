a52133b1618f0cd95d6eae83f70d1a0c163263d1
The Frog <79814004+jhonnyfreitassa@users.noreply.github.com>
2026-01-14 13:35:18 -0300

    Update service worker cache version to v45

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Incremente a versão sempre que fizer alterações nos arquivos principais
-const CACHE_NAME = "planner-pessoal-cache-v44";
+const CACHE_NAME = "planner-pessoal-cache-v45";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -55,6 +55,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
