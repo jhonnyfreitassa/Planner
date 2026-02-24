64c9f8492d2a93525e4b18b7f8b16a126c99dea3
The Frog <79814004+jhonnyfreitassa@users.noreply.github.com>
2026-02-24 13:01:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Versão 2.8 
-const CACHE_NAME = "planner-2026-v2.8"; 
+// Versão 2.9 
+const CACHE_NAME = "planner-2026-v2.9"; 
 
 const urlsToCache = [
   "./",
@@ -55,6 +55,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
